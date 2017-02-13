a2683d1f5655b4d52eab4806083e792805f635c6
Garrett McGrath <Garrett McGrath>
2017-02-12 21:20:01 -0500

    Add pip files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'maxquant-aws-script',
+  packages = ['maxquant-aws-script'],
+  version = '0.1',
+  description = '',
+  author = 'Brenden Judson',
+  author_email = 'bjudson1@nd.edu',
+  url = 'https://github.com/bjudson1/maxquant-aws-script',
+  download_url = 'https://github.com/bjudson1/maxquant-aws-script/tarball/0.1',
+  keywords = ['maxquant', 'aws', 'script'],
+  classifiers = [],
+)
